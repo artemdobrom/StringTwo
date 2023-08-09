f82866e0ec312dff7ed3f2df1b4d696dc662a56e
artjom <artemdobrom@gmail.com>
2023-08-09 23:29:28 +0300

    2 часть готова, тут есть Метод chartAt для поиска симфола по индексу. replace для удаление или замены значения и equals для сравнения

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,35 @@
 public class Main {
     public static void main(String[] args) {
         String phoneNumber = "9604157537";
-        if (phoneNumber.length()==10) {                                         //у строки тоже есть функия ленг,но со скобками
-                                                       //CTRL+ALT+T - и строчка от которой мы хотим сделать оператор иф
-            phoneNumber = '7' + phoneNumber;          //этим кодом мы сказали, что если количесво цифр 10, тогда нужно прибавить на конце цифру "7"
+
+        phoneNumber = phoneNumber.replace("-", "");    //тут мы удаляем все "-", которые могуть быть
+        phoneNumber = phoneNumber.replace(" ", "");  // тут удалили все пробелы
+        phoneNumber = phoneNumber.replace("_", "");
+        phoneNumber = phoneNumber.replace("+", "");
+
+
+                                                     //у строки тоже есть функия ленг,но со скобками
+        if (phoneNumber.length()==10) {              //CTRL+ALT+T - и строчка от которой мы хотим сделать оператор иф
+//            phoneNumber = '7' + phoneNumber;          //этим кодом мы добавили в начало, если количесво цифр 10, тогда нужно прибавить на конце цифру "7"
+
 
         } else if (phoneNumber.length() > 11) {       //тут мы доворим, что если номер длинее стандартного, тогда должна вылести ошибка
             throw new RuntimeException("Телеофон слишком длинный");    // команда для Ошибки с красными буквами в ИДЕИ "throw new RuntimeException()"
         } else if (phoneNumber.length() < 10) {
-            throw new RuntimeException("Телефон слишком короткий");
+            throw new RuntimeException("Телефон слишком короткий");  //метод chartAt и в скобках тот индекс, который нас интересует (отчёт идёт с 0)
+        } else if (phoneNumber.length() == 11 && phoneNumber.charAt(0) != '7') {
+            throw new RuntimeException("Первая цифра не '7'");
         }
 
-
         System.out.println("phoneNumber = " + phoneNumber);
+
+        String expectedPhone = "79604157537";    //Образец правильной формы ввода номера
+        if (phoneNumber.equals(expectedPhone)) {
+            System.out.println("Успех");
+        } else {
+            System.out.println("Номер задан в не корректной форме");
+        }
+
 
     }
 }
